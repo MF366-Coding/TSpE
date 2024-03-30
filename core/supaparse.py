ea0e03b65084c74015aaf374b7cf18a9bf015f19
MF366 <real_mf366@yahoo.com>
2024-03-30 20:10:18 +0000

    screen is extremely broken; we'll try 2 fix on next commit

---
+++ core/supaparse.py
@@ -0,0 +1,185 @@
+"""
+supaparse.py
+
+This module allows you to read, interpret and write to SP and DAT files.
+"""
+
+
+def read_sp_file(filename: str) -> bytearray:
+    """
+    read_sp_file allows for storing the contents of an SP file as a bytearray
+
+    Args:
+        filename (str): the path to the file
+
+    Returns:
+        bytearray: the contents of the SP file
+    """
+    
+    with open(filename, 'rb') as f:
+        data = bytearray(f.read())
+    
+    return data
+
+
+def interpret_sp_data(data: bytearray) -> dict[str, bytearray]:
+    """
+    interpret_sp_data interprets the contents of a SP file
+
+    Args:
+        data (bytearray): the contents of the SP file
+
+    Returns:
+        dict[str, bytearray]: relation key - value, with strings as keys, bytearrays as values
+    """
+    
+    level: dict[str, bytearray] = {
+        'level': data[:1440],
+        'unused': data[1440:1444],
+        'initial_gravitation': data[1444],
+        'space': data[1445],
+        'level_title': data[1446:1469],
+        'initial_freeze_zonks': data[1469],
+        'number_of_infotrons_needed': data[1470],
+        'number_of_special_ports': data[1471],
+        'special_port1': data[1472:1478],
+        'special_port2': data[1478:1484],
+        'special_port3': data[1484:1490],
+        'special_port4': data[1490:1496],
+        'special_port5': data[1496:1502],
+        'special_port6': data[1502:1508],
+        'special_port7': data[1508:1514],
+        'special_port8': data[1514:1520],
+        'special_port9': data[1520:1526],
+        'special_port10': data[1526:1532]
+    }
+    
+    return level
+
+
+def write_sp_file(filename: str, level: dict[str, bytearray]):
+    data = bytearray()
+    
+    for value in level.values():
+        data.extend(value)
+
+    with open(filename, 'wb') as f:
+        f.write(data)
+
+
+def read_dat_file(filename: str):
+    with open(filename, 'rb') as f:
+        data = bytearray(f.read())
+
+    return data
+
+
+def interpret_dat_data(data: bytearray) -> list:    
+    levels: list[dict[str, bytearray]] = []
+    
+    for i in range(0, len(data), 1536):
+        level_data: bytearray = data[i:i+1536]
+        
+        level: dict[str, bytearray] = {
+            'level': level_data[:1440],
+            'unused': level_data[1440:1444],
+            'initial_gravitation': level_data[1444],
+            'space': level_data[1445],
+            'level_title': level_data[1446:1469],
+            'initial_freeze_zonks': level_data[1469],
+            'number_of_infotrons_needed': level_data[1470],
+            'number_of_special_ports': level_data[1471],
+            'special_port1': level_data[1472:1478],
+            'special_port2': level_data[1478:1484],
+            'special_port3': level_data[1484:1490],
+            'special_port4': level_data[1490:1496],
+            'special_port5': level_data[1496:1502],
+            'special_port6': level_data[1502:1508],
+            'special_port7': level_data[1508:1514],
+            'special_port8': level_data[1514:1520],
+            'special_port9': level_data[1520:1526],
+            'special_port10': level_data[1526:1532]
+        }
+        
+        levels.append(level)
+        
+    if len(levels) > 111:
+        levels = levels[:111]
+        
+    elif len(levels) < 111:
+        for i in range(0, 111 - len(levels)):
+            levels.append({
+                'level': bytearray([0] * 1440),
+                'unused': bytearray([0] * 4),
+                'initial_gravitation': bytearray([0]),
+                'space': bytearray([0]),
+                'level_title': bytearray([0] * 23),
+                'initial_freeze_zonks': bytearray([0]),
+                'number_of_infotrons_needed': bytearray([0]),
+                'number_of_special_ports': bytearray([0]),
+                'special_port1': bytearray([0] * 6),
+                'special_port2': bytearray([0] * 6),
+                'special_port3': bytearray([0] * 6),
+                'special_port4': bytearray([0] * 6),
+                'special_port5': bytearray([0] * 6),
+                'special_port6': bytearray([0] * 6),
+                'special_port7': bytearray([0] * 6),
+                'special_port8': bytearray([0] * 6),
+                'special_port9': bytearray([0] * 6),
+                'special_port10': bytearray([0] * 6)
+            })
+
+    return levels
+
+
+def write_dat_file(filename: str, levels: list[dict[str, bytearray]]):
+    data = bytearray()
+    
+    for level in levels:
+        for level_data in level.values():
+            data.extend(level_data)
+    
+    with open(filename, 'wb') as f:
+        f.write(data)
+
+
+def string_to_bytes(string: str) -> list[int]:
+    return [ord(i) for i in string]
+    
+
+def bytes_to_string(bytes_iterable) -> str:
+    return "".join([chr(i) for i in bytes_iterable])
+    
+    
+def string_to_bytetitle(title: str, **options) -> list[int]:
+    fillchar = options.get("fillchar", "-")
+
+    title = title.upper().strip()
+    
+    if len(title) > 23:
+        raise ValueError("title too long (max 23 characters)")
+        
+    title = title.center(23, fillchar)
+    
+    if title[-1] != fillchar and title[0] == fillchar:
+        title = title[0:22] + fillchar
+    
+    return string_to_bytes(title)
+
+
+def calculate_special_port_pos(hi: int, lo: int) -> tuple[int, int]:
+    offset: float = (256 * hi + lo) / 2
+    
+    y = int(offset / 60)
+    x = int(offset - 60 * y)
+    
+    return x, y
+
+
+def calculate_special_port_hi_lo(x: int, y: int) -> tuple[int, int]:
+    value = ((y * 60) + x) * 2
+    
+    hi = int(value / 256)
+    lo = int(value % 256)
+    
+    return hi, lo
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-from core import screen, settings
+from colorama import Style
+from core import screen, settings, supaparse
 
-program_screen = screen.Screen(screen.program_data, settings.parse_colormap())
+program_data = {
+    "ascii": ['MMP""MM""YMM  .M"""bgd        `7MM"""YMM  ',
+              'P\'   MM   `7 ,MI    "Y          MM    `7  ',
+              '     MM      `MMb.   `7MMpdMAo. MM   d    ',
+              '     MM        `YMMNq. MM   `Wb MMmmMM    ',
+              '     MM      .     `MM MM    M8 MM   Y  , ',
+              '     MM      Mb     dM MM   ,AP MM     ,M ',
+              '   .JMML.    P"Ybmmd"  MMbmmd\'.JMMmmmmMMM ',
+              '                       MM                 ',
+              '                     .JMML.               '],
+    "name": "TSpE",
+    "author": "MF366",
+    "copyright": "Copyright (C) 2024  MF366",
+    "description": "Terminal Supaplex Editor"
+}
+
+print(Style.RESET_ALL)
