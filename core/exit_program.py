72782b4620f1a88861441fa3dd5bc9cf8686bddf
MF366 <real_mf366@yahoo.com>
2024-05-03 18:32:06 +0100

    Got rid of reminders

--- core/exit_program.py
+++ core/exit_program.py
@@ -8,6 +8,7 @@ from typing import Any
 from core import settings
 import sys
 
+
 def leave_program(parser: settings.SettingsParser, code: Any = None):
     parser.save_settings()
     sys.exit(code)
--- main.py
+++ main.py
@@ -10,7 +10,12 @@ import grid
 # [*] FINISH THE GODDAMN OUTSIDE FUNCTIONS AND TEST THE DAMN SCREEN!
 # TODO
 
+
+class ReminderError(Exception): ...
+
+
 FIXME = grid.CHANGE_ME_LATER
+RENDER_CONTEXT = '!/CURRENTRENDERCONTEXTASISNOCHANGE/'
 
 program_data = {
     "ascii": ['MMP""MM""YMM  .M"""bgd        `7MM"""YMM  ',
@@ -31,7 +36,7 @@ program_data = {
 
 if len(sys.argv) > 1:
     PARSER = settings.SettingsParser(sys.argv[1])
-    
+
 else:
     PARSER = settings.SettingsParser(os.path.join(os.path.dirname(__file__), 'core', 'settings.json'))
 
@@ -41,58 +46,99 @@ home_scrn = screen.Context('Home Screen', FIXME)
 
 
 # [*] Home screen setup
+def about_tspe():
+    return f"TSpE - Terminal Supaplex Editor is a backwards-compatible Supaplex level editor made by MF366\n\n{RENDER_CONTEXT}"
+
+
 def change_directory(path: str):
     global cur_dir
-    
+
     if not os.path.exists(path):
         raise FileNotFoundError('the selected path does not exist')
-    
+
     if path == '..':
         cur_dir = os.path.dirname(cur_dir)
-    
+
     elif path == '~':
         cur_dir = os.path.expanduser('~')
-        
+
     else:
         cur_dir = path
-        
-    return f"Working Directory is now set to: {cur_dir}\n\n!/CURRENTRENDERCONTEXTASISNOCHANGE/"
+
+    return f"Working Directory is now set to: {cur_dir}\n\n{RENDER_CONTEXT}"
 
 
 def delete_file_or_folder(path: str):
     if not PARSER.allow_del:
         raise PermissionError('deleting files/directories from the disk is disabled')
-    
+
     exists_as_given: bool = os.path.exists(path)
     exists_as_joint_path: bool = os.path.exists(os.path.join(cur_dir, path))
     path_to_remove = False
-    
+
     if exists_as_given:
         path_to_remove = path
-        
-        if os.path.isdir(path_to_remove):
-            os.removedirs(path_to_remove)
-        
-        else:
-            os.remove(path_to_remove)
-            
+
     elif exists_as_joint_path:
         path_to_remove = os.path.join(cur_dir, path)
-        
-        if os.path.isdir(path_to_remove):
-            os.removedirs(path_to_remove)
-        
-        else:
-            os.remove(path_to_remove)
-    
+
     else:
         raise FileNotFoundError('the selected path does not exist')
-    
-    return f"'{path_to_remove}' sucessfully removed!\n\n!/CURRENTRENDERCONTEXTASISNOCHANGE/"
+
+    if os.path.isdir(path_to_remove):
+            os.removedirs(path_to_remove)
+
+    else:
+        os.remove(path_to_remove)
+
+    return f"'{path_to_remove}' sucessfully removed!\n\n{RENDER_CONTEXT}"
+
+
+def dump_tspe_settings():
+    PARSER.save_reload()
+    return f"Settings loaded!\n\n{RENDER_CONTEXT}"
+
+
+def echo_like_command(what: str, path: str = None):
+    echoed_contents = ' '.join(what.split('\n'))
+
+    if not path:
+        return f"{echoed_contents}\n\n{RENDER_CONTEXT}"
+
+    exists_as_given: bool = os.path.exists(path)
+    exists_as_joint_path: bool = os.path.exists(os.path.join(cur_dir, path))
+    path_to_write = False
+
+    if exists_as_given:
+        path_to_write = path
+
+    elif exists_as_joint_path:
+        path_to_write = os.path.join(cur_dir, path)
+
+    else:
+        raise FileNotFoundError('the selected path does not exist')
+
+    if not os.path.isfile(path_to_write):
+            raise BufferError("can't write to an object that isn't a file")
+
+    else:
+        with open(path_to_write, 'a', encoding='\n') as f:
+            f.write(f'\n{echoed_contents}')
+
+    return f"Appended contents to {path_to_write}\n\n{RENDER_CONTEXT}"
 
 
 home_cd_del_args = [screen.Argument('path')]
-home_commands = [screen.Command('cd', home_cd_del_args, change_directory), screen.Command('delete', home_cd_del_args, delete_file_or_folder), screen.Command('del', home_cd_del_args, delete_file_or_folder)]
+home_echo_args = [screen.Argument('what'), screen.OptionalArgument('path', '')]
+
+home_commands = [
+    screen.Command('about', [], about_tspe),
+    screen.Command('cd', home_cd_del_args, change_directory),
+    screen.Command('delete', home_cd_del_args, delete_file_or_folder),
+    screen.Command('del', home_cd_del_args, delete_file_or_folder), # [i] Forgot to implement aliases - too late now - so this is how I'm gonna do it
+    screen.Command('dump', [], dump_tspe_settings),
+    screen.Command('echo', home_cd_del_args, echo_like_command)
+]
 
 home_scrn.add_several_commands(home_commands)
 
